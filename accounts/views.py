1d6ec5dbaa09c21eb67c5ed6d77c79e63214f65b
interntuts <117482814+interntuts@users.noreply.github.com>
2023-03-09 21:50:12 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 # Create your views here.
+
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if len(email) <= 0:
+            message.error('Email id field may not be empty')
+        if len(password) <= 0:
+            message.error('Password field may not be empty')
+ 
+        try:
+            username = User.objects.get(email = email).username
+        except User.DoesNotExist:
+            messages.error(request, "Invalid email id")
+            return redirect('login')
+            
+        if username:
+            user = auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.error(request, "Invalid password")
+                return redirect('login')
+        else:
+            messages.error(request, "Invalid email id")
+            
+    return render(request, 'login.html')
